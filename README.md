f526f93bc7a44678d75c70fa0888ff9d9f490a40
Brian Loveless <Brian.loveless1@yahoo.com>
2020-01-01 12:06:54 -0500

    start

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # flip
+
 flip the card game
+*****
+
+Making a game where you can flip one card at a time or both.
+
+High card wins.
+
+Kinda like flipping a coin, but with extra steps.
+
+Can flip one card at a time or both together.
+
+B-Love 2020!
--- card_flip/game.js
+++ card_flip/game.js
@@ -1,1 +1,15 @@
 console.log("testing");
+
+/// lets start with just "face cards"
+// Highest "A" "K" "Q" "J" "10" Lowest
+
+function cardOneFlip() {
+    // rndm number between 1 and 6
+  let x = Math.floor(Math.random() * 6) + 1;
+  console.log(x);
+}
+
+function cardTwoFlip() {
+  let y = Math.floor(Math.random() * 6) + 1;
+  console.log(y);
+}
